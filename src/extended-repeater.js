0ba300d846b1e47e92154e2cae72814ed659ed28
Anniyak <57433322+Anniyak@users.noreply.github.com>
2021-09-28 17:46:16 +0300

    feat: solve repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -14,8 +14,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
+ * 	`options` это **объект** опций, который содержит следующие свойства:
+  -	`repeatTimes` устанавливает число повторений `str`
+  - `separator` это строка, разделяющая повторения `str`
+  - `addition` это дополнительная строка, которая будет добавлена после каждого повторения `str`
+  - `additionRepeatTimes` устанавливает число повторений `addition`
+  - `additionSeparator` это строка, разделяющая повторения `addition`
+ * Например: `repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })` 
+  => `'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'`
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+export default function repeater(str, options) {
+  let answer = '';
+  let strL = str !== undefined ? (str !== null ? String(str) : 'null') : '';
+  let repeatTimes = options.repeatTimes || 0;
+  let separator = options.separator || '+';
+  let addition = options.addition !== undefined ? (options.addition !== null ? String(options.addition) : 'null') : '';
+  let additionRepeatTimes = options.additionRepeatTimes || 0;
+  let additionSeparator = options.additionSeparator || '|';
+  if (repeatTimes == 0) return strL + (additionRepeatTimes == 0 ? addition : '');
+  for (let i = 0; i < repeatTimes; i++) {
+    if (i != 0) answer += separator;
+    answer += strL;
+    let sep = '';
+    for (let t = 0; t < additionRepeatTimes; t++) {
+      if (t != 0) sep += additionSeparator;
+      sep += addition;
+    }
+    if (additionRepeatTimes == 0) {
+      sep += addition;
+    }
+    answer += sep;
+  }
+  return answer;
 }
